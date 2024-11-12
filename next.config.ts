c2ad733ddcb02084ccfa28ae82c96ee354e9bb7e
Oren <78069187+oomari@users.noreply.github.com>
2024-11-12 15:17:13 -0500

    Updated next.config.ts file with appropriate Domains

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "s.yimg.com",
+      "static.politico.com",
+      "dims.apnews.com",
+      "assets2.cbsnewsstatic.com",
+      "fortune.com",
+      "media-cldnry.s-nbcnews.com",
+      "nypost.com",
+      "www.investors.com",
+      "www.onlineathens.com",
+      "a1.espncdn.com",
+      "images.deccanherald.com",
+      "images.pushsquare.com",
+      "cdn.mos.cms.futurecdn.net",
+      "www.reuters.com",
+      "kubrick.htvapps.com",
+      "images.axios.com",
+      "d3i6fh83elv35t.cloudfront.net",
+    ],
+  },
 };
 
 export default nextConfig;
