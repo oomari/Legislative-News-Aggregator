69a1295c134504eadc7c7a70b15873bd3fcc2890
Oren <78069187+oomari@users.noreply.github.com>
2024-11-12 15:12:39 -0500

    Improved NavBar formatting

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,13 +10,13 @@ export default function Navbar() {
           <Image src={"/logo-sa.jpg"} alt="logo" width={250} height={250} />
         </Link>
         <div className="flex gap-4 pr-10 text-white">
-          <button className="text-[#82B9FB] hover:text-blue-600 transition-all ease-in-out font-bold border-transparent opacity-50 bg-gray-700 rounded-md p-4">
+          <button className="text-sm text-[#6da8f0] hover:text-blue-600 transition-all ease-in-out font-bold border-transparent opacity-60 bg-gray-700 rounded-md p-4">
             Request a Demo
           </button>
-          <button className="font-bold transition-all ease-in-out hover:text-blue-600">
+          <button className="text-sm font-bold transition-all ease-in-out hover:text-blue-600">
             Pro Features
           </button>
-          <button className="font-bold transition-all ease-in-out hover:text-blue-600">
+          <button className="text-sm font-bold transition-all ease-in-out hover:text-blue-600">
             Log In
           </button>
         </div>
