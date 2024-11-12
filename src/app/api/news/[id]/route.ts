f7576c8fd009d095234e3a6e5f5dd97540774cb7
Oren <78069187+oomari@users.noreply.github.com>
2024-11-12 15:14:30 -0500

    Added comments

--- src/app/api/news/[id]/route.ts
+++ src/app/api/news/[id]/route.ts
@@ -1,9 +1,13 @@
 import { prisma } from "@/modules/prisma";
+
+// GET /api/news/[id]
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
+  // pull news id from params
   const { id } = params;
+  // using the id, fetch article from database with corresponding id
   const newsArticle = await prisma.newsArticle.findFirst({
     where: { id: parseInt(id) },
   });
