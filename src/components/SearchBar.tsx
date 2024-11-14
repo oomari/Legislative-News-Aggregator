140cfe9ffbdeed27b298599de1735fa5f23de3cb
Oren <78069187+oomari@users.noreply.github.com>
2024-11-14 12:34:06 -0500

    Connected SearchBar to keyword filtering

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function SearchBar({
+  onChange,
+}: {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+  return (
+    <div className="w-full flex flex-col ">
+      <input
+        type="text"
+        placeholder="Search..."
+        className="w-full rounded-sm p-2"
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
