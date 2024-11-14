41d36dc2f8161f18fb4985a0349a13fe90a130fd
Oren <78069187+oomari@users.noreply.github.com>
2024-11-14 12:43:42 -0500

    called fetchArticles in seperate files to be able to import and call it when Get New Articles button is clicked

--- src/scripts/fetch-articles.ts
+++ src/scripts/fetch-articles.ts
@@ -1,50 +1,6 @@
-const NEWS_API_KEY = "a78455865a3b4e77a44f5ab2458ed238";
-const NewsApi = require("newsapi");
-import { prisma } from "../modules/prisma";
 import { loadEnvConfig } from "@next/env";
-const STATES = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE"];
-const TOPICS = ["politics", "sports", "entertainment", "technology"];
+import { fetchArticles } from "./functions/fetch-articles";
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
-type NewsApiResponse = {
-  status: string;
-  totalResults: number;
-  articles: NewsApiArticle[];
-};
-
-type NewsApiArticle = {
-  source: { id: string; name: string };
-  author: string;
-  title: string;
-  description: string;
-  url: string;
-  urlToImage: string;
-  publishedAt: string;
-  content: string;
-};
-
-(async () => {
-  const newsapi = new NewsApi(NEWS_API_KEY);
-
-  const resp: NewsApiResponse = await newsapi.v2.topHeadlines({
-    country: "us",
-  });
-
-  const articles = resp.articles;
-
-  // delete all
-  await prisma.newsArticle.deleteMany({});
-
-  await prisma.newsArticle.createMany({
-    data: articles.map((article) => ({
-      title: article.title,
-      description: article.description,
-      imageUrl: article.urlToImage,
-      publishedAt: new Date(article.publishedAt),
-      state: STATES[Math.floor(Math.random() * STATES.length)],
-      topic: TOPICS[Math.floor(Math.random() * TOPICS.length)],
-      articleUrl: article.url,
-    })),
-  });
-})();
+fetchArticles();
